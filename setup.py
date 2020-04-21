0fa442b36ac024cc20468a6430c1cbcea75477b7
sgmoorthy <sgmoorthy@gmail.com>
2020-04-21 17:35:55 +0100

    folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine learning model to build & train voice based experiments',
+    author='sgmoorthy',
+    license='MIT',
+)
